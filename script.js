641f4d248f624ebe407b74eef711f83efd204cd1
RemmyKyalo <chaloremmy@gmail.com>
2024-01-12 23:42:17 +0300

    added toggle button  functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const menuBtn= document.getElementsByClassName('menu-btn'); 
+const  navLinks =document.getElementById('nav-links');
+const menuIcon = document.querySelector("i")
+
+menuBtn.addEventListener('click',() =>{ 
+  
+navLinks.classList.toggle("open");
+
+const isOpen = navLinks.classList.contains("open");
+
+menuIcon.setAttribute("class",isOpen ? "ri-close-line" :"ri-menu-line");
+});
+
