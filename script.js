6f72f5ac4fc64a3153160d4d17722be338abf7ae
RemmyKyalo <chaloremmy@gmail.com>
2024-01-13 03:05:11 +0300

    refactored my code for responsiveness

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const menuBtn= document.getElementsByClassName('menu-btn'); 
+const menuBtn= document.getElementById('menu-btn'); 
 const  navLinks =document.getElementById('nav-links');
-const menuIcon = document.querySelector("i")
+const menuIcon = menuBtn.querySelector("i")
 
 menuBtn.addEventListener('click',() =>{ 
-  
+
 navLinks.classList.toggle("open");
 
 const isOpen = navLinks.classList.contains("open");
 
 menuIcon.setAttribute("class",isOpen ? "ri-close-line" :"ri-menu-line");
 });
-
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuIcon.setAttribute("class", "ri-menu-line");
+});
